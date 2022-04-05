b2bbf998c2fc565dd0208b9b6c11c8de7e41dfa5
Edward Leno <leno_edward@bah.com>
2022-04-05 12:09:37 -0400

    initial checkin

---
+++ src/main/java/fwb_test/fwb_test/App.java
@@ -0,0 +1,13 @@
+package fwb_test.fwb_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fwb_test/fwb_test/AppTest.java
@@ -0,0 +1,28 @@
+package fwb_test.fwb_test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class AppTest
+{
+	@Order(1)
+	@Test
+	public void Simple()
+	{
+		System.out.println ("EJL - inside Simple test");
+		
+		ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        WebElement p = driver.findElement(By.name("q"));
+        p.sendKeys("Selenium Java");
+        p.submit();
+        System.out.println ("EJL - title = " + driver.getTitle());
+	}
+}
